b2154ead7992e5f28dab60149d6cad6cca8c5d0e
Tuna Ayberk Ozmen <tunaayberk@hotmail.com>
2020-04-07 18:39:22 -0400

    first commit

---
+++ precache-manifest.8cdcdde9f5110488e6220cf31ceedd67.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6820ab4634e77ad989bb6341b256435d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99faf593030edfe78a92",
+    "url": "/static/css/11.e0bf51f8.chunk.css"
+  },
+  {
+    "revision": "89285103e89b971aeb82",
+    "url": "/static/css/7.8182a111.chunk.css"
+  },
+  {
+    "revision": "adcd203cc3c7cad618a8",
+    "url": "/static/css/main.253bce10.chunk.css"
+  },
+  {
+    "revision": "36f971061072c48543f0",
+    "url": "/static/js/0.09fe7cb1.chunk.js"
+  },
+  {
+    "revision": "c314054a498e7d69116e32d84cd80060",
+    "url": "/static/js/0.09fe7cb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "314b660391e8eabc92a5",
+    "url": "/static/js/1.03778ce1.chunk.js"
+  },
+  {
+    "revision": "97913bff43dddd501eda",
+    "url": "/static/js/10.07dd4fbe.chunk.js"
+  },
+  {
+    "revision": "99faf593030edfe78a92",
+    "url": "/static/js/11.ace4a528.chunk.js"
+  },
+  {
+    "revision": "633668690b4f51b5391a",
+    "url": "/static/js/12.fdd84a4b.chunk.js"
+  },
+  {
+    "revision": "843d1dcaa7af984e3371",
+    "url": "/static/js/13.985361b0.chunk.js"
+  },
+  {
+    "revision": "f53c2dbd590748e3ca85",
+    "url": "/static/js/14.97d486ed.chunk.js"
+  },
+  {
+    "revision": "6bd8a631244af5448b48",
+    "url": "/static/js/15.17894565.chunk.js"
+  },
+  {
+    "revision": "c97f642212d5ee4da063",
+    "url": "/static/js/4.dd8ac192.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/4.dd8ac192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5de5d72be17b351cfc7",
+    "url": "/static/js/5.195951a5.chunk.js"
+  },
+  {
+    "revision": "cc86c8e0a9f5dadf7b38",
+    "url": "/static/js/6.3761c096.chunk.js"
+  },
+  {
+    "revision": "89285103e89b971aeb82",
+    "url": "/static/js/7.5751e459.chunk.js"
+  },
+  {
+    "revision": "f30a762814b9ef906164",
+    "url": "/static/js/8.8bf20495.chunk.js"
+  },
+  {
+    "revision": "8d7f0e95b76ac69f077a",
+    "url": "/static/js/9.69a3b591.chunk.js"
+  },
+  {
+    "revision": "adcd203cc3c7cad618a8",
+    "url": "/static/js/main.f6eb2ce3.chunk.js"
+  },
+  {
+    "revision": "4c5278f7ec6006b923cf",
+    "url": "/static/js/runtime-main.0209cd42.js"
+  },
+  {
+    "revision": "72cda1a6d73a751126c8ac6a5b196bd6",
+    "url": "/static/media/aktiffelsefe.72cda1a6.jpg"
+  },
+  {
+    "revision": "75a3bacc8da2e8bb97740da51947407e",
+    "url": "/static/media/ankara.75a3bacc.jpg"
+  },
+  {
+    "revision": "78d6c7b40a511442d826bcf949a6d97f",
+    "url": "/static/media/ankara.78d6c7b4.jpg"
+  },
+  {
+    "revision": "e48c4be043ae9a773a850e5288737e5a",
+    "url": "/static/media/balodana.e48c4be0.png"
+  },
+  {
+    "revision": "2714f538d4ed33a9383c3a7344c2051a",
+    "url": "/static/media/beforeafter.2714f538.jpg"
+  },
+  {
+    "revision": "cd8caaa6f0e1db8e2373ad737ce46f40",
+    "url": "/static/media/bomar.cd8caaa6.jpg"
+  },
+  {
+    "revision": "5eb838854dadbc8e004c71bede84c4ad",
+    "url": "/static/media/decorilloCal.5eb83885.jpg"
+  },
+  {
+    "revision": "aaf1005b9513ee12a08e52113c2274dd",
+    "url": "/static/media/decorilloMail.aaf1005b.jpg"
+  },
+  {
+    "revision": "6bf5a07c05fcfd625434a41e7a4284a9",
+    "url": "/static/media/decorilloMail2.6bf5a07c.jpg"
+  },
+  {
+    "revision": "e1c384163f0e0d5c9ca53aff80361b77",
+    "url": "/static/media/decorilloMail3.e1c38416.jpg"
+  },
+  {
+    "revision": "597c98fd6b9b4d77425d29d451d91a1a",
+    "url": "/static/media/decorilloNew.597c98fd.jpg"
+  },
+  {
+    "revision": "a828c805ce075e241f8f145f31c85a6c",
+    "url": "/static/media/dfs.a828c805.png"
+  },
+  {
+    "revision": "36c9e60c94126c623bd8b44b6856100a",
+    "url": "/static/media/diaspora.36c9e60c.jpg"
+  },
+  {
+    "revision": "42ddd3fa5e504bf732cb5f1cecaa9166",
+    "url": "/static/media/diaspora.42ddd3fa.png"
+  },
+  {
+    "revision": "c67f923e5f09f565f4edc1218b66b4a8",
+    "url": "/static/media/dorakapi.c67f923e.jpg"
+  },
+  {
+    "revision": "b5241e31d8e5637621f186c4c8e1ad39",
+    "url": "/static/media/finance.b5241e31.png"
+  },
+  {
+    "revision": "0f8effeb37c5bfbb708ea316412ebece",
+    "url": "/static/media/fmkbrochure.0f8effeb.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a35fd21832956a1df3e8afd97e451aac",
+    "url": "/static/media/gencken.a35fd218.jpg"
+  },
+  {
+    "revision": "8b2c4794152cdad435ab08d6aecb1a56",
+    "url": "/static/media/genckenSocial.8b2c4794.jpg"
+  },
+  {
+    "revision": "e60b6627d6cd5540d9a0508e94537573",
+    "url": "/static/media/genckenSocial2.e60b6627.jpg"
+  },
+  {
+    "revision": "d16bdc1dbe55f1d4487f60337e0562b6",
+    "url": "/static/media/genckenfest.d16bdc1d.jpg"
+  },
+  {
+    "revision": "ccb5b9fa944badac919d515257da4a61",
+    "url": "/static/media/hacettepe.ccb5b9fa.jpg"
+  },
+  {
+    "revision": "37b92e5c35c4e7a4a6487c5f9d3c5aa8",
+    "url": "/static/media/home_mini_bg.37b92e5c.jpg"
+  },
+  {
+    "revision": "d8029e4f0aae2269cf4155d612d81bfe",
+    "url": "/static/media/kacis-film.d8029e4f.jpg"
+  },
+  {
+    "revision": "398e64fc0c8c816d46369deda1732b44",
+    "url": "/static/media/kacis-film2.398e64fc.jpg"
+  },
+  {
+    "revision": "8d490ecf2f506e5a47f505f9f6460bf5",
+    "url": "/static/media/kacis-film3.8d490ecf.jpg"
+  },
+  {
+    "revision": "c2cec4b260edf0060460605760ddaf2f",
+    "url": "/static/media/kacis-film3.c2cec4b2.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "fed0884fe62a72c3ca6c11a8abb698ce",
+    "url": "/static/media/po.fed0884f.png"
+  },
+  {
+    "revision": "230035e8d8ee18e39c435bab3a2d306f",
+    "url": "/static/media/questor.230035e8.jpg"
+  },
+  {
+    "revision": "6f5c64d73f79dbaebb98308f64914729",
+    "url": "/static/media/questor.6f5c64d7.jpg"
+  },
+  {
+    "revision": "f959ef008cf978af84a716ac48b74d70",
+    "url": "/static/media/samopar.f959ef00.jpg"
+  },
+  {
+    "revision": "feb0ef10f9e6773b919c316f2d526227",
+    "url": "/static/media/sky.feb0ef10.jpg"
+  },
+  {
+    "revision": "0f9168e76012ccff8f8fda89c30b2cc9",
+    "url": "/static/media/ted.0f9168e7.jpg"
+  },
+  {
+    "revision": "405e9c762feb40c29b3047bf3ce065a8",
+    "url": "/static/media/ted.405e9c76.jpg"
+  },
+  {
+    "revision": "1db68b88e8fd2078063f46acf2d8e0cb",
+    "url": "/static/media/todo.1db68b88.png"
+  },
+  {
+    "revision": "174dfdc87b5884fa2847db60b66d38a6",
+    "url": "/static/media/tuna.174dfdc8.png"
+  },
+  {
+    "revision": "92134a7508d28d38391638ca35e99ad9",
+    "url": "/static/media/turkkan.92134a75.jpg"
+  },
+  {
+    "revision": "5a5075d62e7701a5fc909ccad80351e1",
+    "url": "/static/media/unsplash.5a5075d6.png"
+  }
+]);
